c2b094905b64c092ee356ae97f042b7ba852ac98
anandhu <anandhu1102002@gmail.com>
2023-09-18 13:34:14 +0530

    add

---
+++ conditional.py
@@ -0,0 +1,59 @@
+# a=int(input("enter first number:"))
+# b=int (input("enter second no:"))
+# if a>b : 
+#      print(a,"is largest")
+# else :
+#      print(b,"is largest")
+
+
+
+
+
+
+# x=int(input("enter your age"))
+# if(x<18):
+#     print("you are a miner")
+# elif(x>=18 and x<65):
+#     print("you are a adult")
+# else:
+#     print("you are senior cityzen")    
+
+
+
+
+
+
+
+# x=int(input("enter first no:"))
+# y=int(input("enter second no:"))
+# if(x%y==0):
+#     print(x,"is divided by",y)
+# else:
+#     print(x,"is not divided",y)
+
+
+
+
+
+
+# m=int (input("Enter first no:"))
+# n=int(input("Enetr second no:"))
+# s=m+n
+# if(s%2==0):
+#     print(s,"is even")
+# else:
+#     print(s,"is odd")
+
+
+
+x=input("enter a letter:")
+v=("a","e","i","o","u","A","E","I","O","U")
+if(x in v):
+    print(x,"is a vowel")
+else :
+    print(x,"is a consonant")
+
+
+
+
+
